d9357637e07f37c56689d7991d15dae1e6d6b87c
guilherme-fittipaldi <guilherme.fittipaldi6@gmail.com>
2021-03-26 19:17:37 -0300

    alteração nas urls

--- hackathon/home/urls.py
+++ hackathon/home/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, CategoryView
 
 urlpatterns = [
     #path('', views.home, name='home')
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add-post'),
-     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update-post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update-post'),
+    path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete-post'),
+    path('category/<str:cats>/', CategoryView, name='category'),
 ]
